a9bcedd027b2ab2de164be02b2a46b103751baa2
m21tomas <94525145+m21tomas@users.noreply.github.com>
2024-05-02 08:34:32 +0300

    Delete Fambruhrefral2 directory
    It's obsolete and deprecated. It has corrupted code that doesn't work right.

--- Fambruhrefral2/src/main/java/coms/repository/DetailImageRepo.java
+++
@@ -1,9 +0,0 @@
-package coms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import coms.model.product.ProductImageDetail;
-
-public interface DetailImageRepo extends JpaRepository<ProductImageDetail, Long> {
-   
-}
